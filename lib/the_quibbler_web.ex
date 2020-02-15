d2d198d17c6635fe55ed49cd5bd08d7127ab4741
Pedro Medeiros <pedrosnk@gmail.com>
2020-02-14 19:55:48 -0500

    Update dependences

--- lib/the_quibbler_web.ex
+++ lib/the_quibbler_web.ex
@@ -43,7 +43,8 @@ defmodule TheQuibblerWeb do
       import TheQuibblerWeb.ErrorHelpers
       import TheQuibblerWeb.Gettext
       alias TheQuibblerWeb.Router.Helpers, as: Routes
-      import Phoenix.LiveView, only: [live_render: 2, live_render: 3, live_link: 1, live_link: 2]
+
+      import Phoenix.LiveView.Helpers
     end
   end
 
